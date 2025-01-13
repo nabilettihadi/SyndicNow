b6c4bbdb5b5bfbba52d7eeea5100afa19f058b9f
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2025-01-13 10:04:51 +0100

    Initialisation du projet Spring Boot pour l'application SyndicNow et Configuration de base avec Spring Web, Spring Data JPA et PostgreSQL.

---
+++ src/main/java/ma/Nabil/SyndicNow/SyndicNowApplication.java
@@ -0,0 +1,13 @@
+package ma.Nabil.SyndicNow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyndicNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyndicNowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/Nabil/SyndicNow/SyndicNowApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.Nabil.SyndicNow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyndicNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

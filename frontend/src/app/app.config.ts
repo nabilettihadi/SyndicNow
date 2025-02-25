7d92cac120c846f80705eca6e539d1db6de13c31
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2025-02-25 09:58:50 +0000

    Initial Angular project setup

---
+++ frontend/src/app/app.config.ts
@@ -0,0 +1,22 @@
+import { ApplicationConfig, isDevMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    })
+  ]
+};

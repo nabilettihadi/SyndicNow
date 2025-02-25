1791c0eff75457f3b34a8d31ef72f6fc7d8d3eb2
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2025-02-25 10:13:39 +0000

    [auth] Add auth module with routing configuration

---
+++ frontend/src/app/features/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { authReducer } from './store/reducers';
+import { AuthEffects } from './store/effects/auth.effects';
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    AuthRoutingModule,
+    StoreModule.forFeature('auth', authReducer),
+    EffectsModule.forFeature([AuthEffects])
+  ]
+})
+export class AuthModule { }
